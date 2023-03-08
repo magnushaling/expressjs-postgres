d14503cf8ade67e96066a7256a18ee92129720a4
magnushaling <122467690+magnushaling@users.noreply.github.com>
2023-03-08 19:37:41 +0200

    03

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import express from "express";
+import cors from "cors";
+import pg from "pg";
+
+// Connect to the database using the DATABASE_URL environment
+//   variable injected by Railway
+//const pool = new pg.Pool();
+
+const app = express();
+const port = process.env.PORT || 3333;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+//  const { rows } = await pool.query("SELECT NOW()");
+  res.send(JSON.stringify({ text: "todo1" }));
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
